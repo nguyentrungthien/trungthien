b5a18cba459d6c234dab645c624845ed17882216
TrungThien <TrungThien@users.noreply.github.com>
2016-11-22 22:14:23 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 ﻿<?php
-$domain= 'http://trungkien.cf'; // không có / ở cuối
-$linktoken= 'http://trungkien.cf/token.txt'; // nên để trong file txt nhé
-$id_like= '100005310551888'; // ID User VIP
+$domain= 'https://github.com/nguyentrungthien/trungthien'; // không có / ở cuối
+$linktoken= 'https://github.com/nguyentrungthien/trungthien/token.txt'; // nên để trong file txt nhé
+$id_like= '100013663542011'; // ID User VIP
 $limitlike= '1000000'; // Số Lượng Like/Status
 ?>
